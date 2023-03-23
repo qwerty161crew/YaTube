a8111fb507d08fafe5ededb3c4ccaf9c5a58897b
Алексей Матвеев <qwerty161crew@gmail.com>
2023-03-23 21:51:42 +0300

    добавил новые маршруты

--- yatube/posts/tests/test_routes.py
+++ yatube/posts/tests/test_routes.py
@@ -16,6 +16,8 @@ CASES = [
     [f'/posts/{POST_ID}/edit/', 'post_edit', [POST_ID]],
     [f'/posts/{POST_ID}/comment/', 'add_comment', [POST_ID]],
     ['/follow/', 'follow_index', None],
+    [f'/profile/{USERNAME}/follow/', 'profile_follow', [USERNAME]],
+    [f'/profile/{USERNAME}/unfollow/', 'profile_unfollow', [USERNAME]]
 ]
 
 
